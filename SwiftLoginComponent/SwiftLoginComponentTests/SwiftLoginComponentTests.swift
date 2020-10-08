697688c72a1f3ae9d64c179d64badaa76c7894bd
Prakash Sengirayar <prakash.fs@gmail.com>
2020-10-08 16:49:46 +0800

    Initial Commit
    Setup Project

---
+++ SwiftLoginComponent/SwiftLoginComponentTests/SwiftLoginComponentTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftLoginComponentTests.swift
+//  SwiftLoginComponentTests
+//
+//  Created by Prakash Sengirayar on 8/10/20.
+//  Copyright © 2020 Prakash Sengirayar. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLoginComponent
+
+class SwiftLoginComponentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
